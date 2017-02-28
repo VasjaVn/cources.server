ac8360b6ed550994645515727329eb50b674165a
VasjaVn <euro2012tovinnitsa@gmail.com>
2017-02-28 10:53:20 +0200

    add handlers for commands of authorization: register, sms_confirm, login

---
+++ softgroup/services/authorization/authorization-impl/src/main/java/com/softgroup/authorization/impl/handler/AuthCmdLoginHandler.java
@@ -0,0 +1,26 @@
+package com.softgroup.authorization.impl.handler;
+
+import com.softgroup.authorization.api.message.LoginRequest;
+import com.softgroup.authorization.api.message.LoginResponse;
+import com.softgroup.authorization.api.router.AuthorizationRequestHandler;
+import com.softgroup.common.protocol.Request;
+import com.softgroup.common.protocol.Response;
+import com.softgroup.common.router.api.AbstractRequestHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthCmdLoginHandler extends AbstractRequestHandler<LoginRequest, LoginResponse>
+        implements AuthorizationRequestHandler
+{
+    private static final String AUTH_CMD_LOGIN_NAME = "login";
+
+    @Override
+    public String getName() {
+        return AUTH_CMD_LOGIN_NAME;
+    }
+    
+    @Override
+    public Response<LoginResponse> handle(Request<?> msg) {
+        return null;
+    }
+}
---
+++ softgroup/services/authorization/authorization-impl/src/main/java/com/softgroup/authorization/impl/handler/AuthCmdSmsConfirmHandler.java
@@ -0,0 +1,26 @@
+package com.softgroup.authorization.impl.handler;
+
+import com.softgroup.authorization.api.message.SmsConfirmRequest;
+import com.softgroup.authorization.api.message.SmsConfirmResponse;
+import com.softgroup.authorization.api.router.AuthorizationRequestHandler;
+import com.softgroup.common.protocol.Request;
+import com.softgroup.common.protocol.Response;
+import com.softgroup.common.router.api.AbstractRequestHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthCmdSmsConfirmHandler extends AbstractRequestHandler<SmsConfirmRequest, SmsConfirmResponse>
+        implements AuthorizationRequestHandler
+{
+    private static final String AUTH_CMD_SMS_CONFIRM_NAME = "sms_confirm";
+
+    @Override
+    public String getName() {
+        return AUTH_CMD_SMS_CONFIRM_NAME;
+    }
+
+    @Override
+    public Response<SmsConfirmResponse> handle(Request<?> msg) {
+        return null;
+    }
+}
