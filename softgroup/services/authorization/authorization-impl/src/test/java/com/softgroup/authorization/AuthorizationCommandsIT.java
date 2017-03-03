b9e905b0e5d314868e496724a881460189c67423
VasjaVn <euro2012tovinnitsa@gmail.com>
2017-03-03 21:49:19 +0200

    changed use class ObjectMapper on class JacksonDataMapper in test method testCommandLoginHandler()

--- softgroup/services/authorization/authorization-impl/src/test/java/com/softgroup/authorization/AuthorizationCommandsIT.java
+++ softgroup/services/authorization/authorization-impl/src/test/java/com/softgroup/authorization/AuthorizationCommandsIT.java
@@ -1,10 +1,10 @@
 package com.softgroup.authorization;
 
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.softgroup.authorization.api.message.LoginRequestData;
 import com.softgroup.authorization.api.message.LoginResponseData;
 import com.softgroup.authorization.impl.handler.AuthCmdLoginHandler;
+import com.softgroup.common.datamapper.JacksonDataMapper;
 import com.softgroup.common.protocol.Request;
 import com.softgroup.common.protocol.Response;
 import org.junit.Test;
@@ -21,12 +21,12 @@ public class AuthorizationCommandsIT {
         String jsonLoginRequest = "{\"header\":{\"uuid\":\"UUID\",\"originUuid\":\"ORIGIN_UUID\",\"command\":\"login\",\"type\":\"authorization\",\"version\":\"v1\"},\"data\":{\"deviceToken\":\"DEVICE_TOKEN_1\"}}";
 
         Request<LoginRequestData> loginRequest
-                = new ObjectMapper().readValue(jsonLoginRequest, new TypeReference<Request<LoginRequestData>>() {});
+                = new JacksonDataMapper().mapData(jsonLoginRequest, new TypeReference<Request<LoginRequestData>>() {});
 
-        Response<LoginResponseData> response = new AuthCmdLoginHandler().handle( loginRequest );
+        Response<LoginResponseData> loginResponse = new AuthCmdLoginHandler().handle( loginRequest );
 
-        assertThat( response.getStatus().getCode(), is(200) );
-        assertThat( response.getStatus().getMessage(), is("OK") );
-        assertThat( response.getData().getToken(), is("TOKEN_1") );
+        assertThat( loginResponse.getStatus().getCode(), is(200) );
+        assertThat( loginResponse.getStatus().getMessage(), is("OK") );
+        assertThat( loginResponse.getData().getToken(), is("TOKEN_1") );
     }
 }
