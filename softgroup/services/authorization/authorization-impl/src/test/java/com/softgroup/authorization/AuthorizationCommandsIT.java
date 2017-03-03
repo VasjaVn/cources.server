da20486bbd3a970ac10689b98e97e0abdfd71dfa
VasjaVn <euro2012tovinnitsa@gmail.com>
2017-03-03 20:48:47 +0200

    add get object login request from string json

--- softgroup/services/authorization/authorization-impl/src/test/java/com/softgroup/authorization/AuthorizationCommandsIT.java
+++ softgroup/services/authorization/authorization-impl/src/test/java/com/softgroup/authorization/AuthorizationCommandsIT.java
@@ -1,5 +1,7 @@
 package com.softgroup.authorization;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.softgroup.authorization.api.message.LoginRequestData;
 import com.softgroup.authorization.api.message.LoginResponseData;
 import com.softgroup.authorization.impl.handler.AuthCmdLoginHandler;
@@ -7,16 +9,21 @@ import com.softgroup.common.protocol.Request;
 import com.softgroup.common.protocol.Response;
 import org.junit.Test;
 
+import java.io.IOException;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class AuthorizationCommandsIT {
 
     @Test
-    public void testCommandLoginHandler() {
-        Request<LoginRequestData> request = new Request<>();
+    public void testCommandLoginHandler() throws IOException {
+        String jsonLoginRequest = "{\"header\":{\"uuid\":\"UUID\",\"originUuid\":\"ORIGIN_UUID\",\"command\":\"login\",\"type\":\"authorization\",\"version\":\"v1\"},\"data\":{\"deviceToken\":\"DEVICE_TOKEN_1\"}}";
 
-        Response<LoginResponseData> response = new AuthCmdLoginHandler().handle( request );
+        Request<LoginRequestData> loginRequest
+                = new ObjectMapper().readValue(jsonLoginRequest, new TypeReference<Request<LoginRequestData>>() {});
+
+        Response<LoginResponseData> response = new AuthCmdLoginHandler().handle( loginRequest );
 
         assertThat( response.getStatus().getCode(), is(200) );
         assertThat( response.getStatus().getMessage(), is("OK") );
