981b6d73b2443a0b7ed89a6e87b4a7d13ba001de
VasjaVn <euro2012tovinnitsa@gmail.com>
2017-03-04 12:33:47 +0200

    convert JSON to Request<?> for testing login command request  handler

--- softgroup/services/authorization/authorization-impl/src/test/java/com/softgroup/authorization/AuthorizationCommandsIT.java
+++ softgroup/services/authorization/authorization-impl/src/test/java/com/softgroup/authorization/AuthorizationCommandsIT.java
@@ -1,7 +1,6 @@
 package com.softgroup.authorization;
 
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.softgroup.authorization.api.message.LoginRequestData;
 import com.softgroup.authorization.api.message.LoginResponseData;
 import com.softgroup.authorization.impl.handler.AuthCmdLoginHandler;
 import com.softgroup.common.datamapper.JacksonDataMapper;
@@ -9,21 +8,18 @@ import com.softgroup.common.protocol.Request;
 import com.softgroup.common.protocol.Response;
 import org.junit.Test;
 
-import java.io.IOException;
-
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class AuthorizationCommandsIT {
 
     @Test
-    public void testCommandLoginHandler() throws IOException {
-        String jsonLoginRequest = "{\"header\":{\"uuid\":\"UUID\",\"originUuid\":\"ORIGIN_UUID\",\"command\":\"login\",\"type\":\"authorization\",\"version\":\"v1\"},\"data\":{\"deviceToken\":\"DEVICE_TOKEN_1\"}}";
+    public void testCommandLoginHandler() {
+        String jsonLoginRequest = "{\"header\":{\"uuid\":\"UUID_1\",\"origin_uuid\":\"ORIGIN_UUID_1\",\"command\":\"login\",\"type\":\"authorization\",\"version\":\"v1\"},\"data\":{\"device_token\":\"DEVICE_TOKEN_1\"}}";
 
-        Request<LoginRequestData> loginRequest
-                = new JacksonDataMapper().mapData(jsonLoginRequest, new TypeReference<Request<LoginRequestData>>() {});
+        Request<?> request = new JacksonDataMapper().mapData(jsonLoginRequest, new TypeReference<Request<?>>() {});
 
-        Response<LoginResponseData> loginResponse = new AuthCmdLoginHandler().handle( loginRequest );
+        Response<LoginResponseData> loginResponse = new AuthCmdLoginHandler().handle( request );
 
         assertThat( loginResponse.getStatus().getCode(), is(200) );
         assertThat( loginResponse.getStatus().getMessage(), is("OK") );
