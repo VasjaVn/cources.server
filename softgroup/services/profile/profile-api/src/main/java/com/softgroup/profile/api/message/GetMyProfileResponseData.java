83a7d8023aa55bb944649a3064b954a3b37a4802
VasjaVn <euro2012tovinnitsa@gmail.com>
2017-03-09 15:07:56 +0200

    add serial version UID

--- softgroup/services/profile/profile-api/src/main/java/com/softgroup/profile/api/message/GetMyProfileResponseData.java
+++ softgroup/services/profile/profile-api/src/main/java/com/softgroup/profile/api/message/GetMyProfileResponseData.java
@@ -3,4 +3,5 @@ package com.softgroup.profile.api.message;
 import com.softgroup.common.protocol.ResponseData;
 
 public class GetMyProfileResponseData implements ResponseData {
+    private static final long serialVersionUID = 3066867066075855991L;
 }
