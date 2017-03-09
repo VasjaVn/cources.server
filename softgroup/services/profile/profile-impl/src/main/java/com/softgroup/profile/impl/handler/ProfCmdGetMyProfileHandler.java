bad694deb130892b89db09287586a909efaa16b8
VasjaVn <euro2012tovinnitsa@gmail.com>
2017-03-09 16:06:30 +0200

    add class handler for command 'get_my_profile'

---
+++ softgroup/services/profile/profile-impl/src/main/java/com/softgroup/profile/impl/handler/ProfCmdGetMyProfileHandler.java
@@ -0,0 +1,27 @@
+package com.softgroup.profile.impl.handler;
+
+import com.softgroup.common.protocol.Request;
+import com.softgroup.common.protocol.Response;
+import com.softgroup.common.router.api.AbstractRequestHandler;
+import com.softgroup.profile.api.message.GetMyProfileRequestData;
+import com.softgroup.profile.api.message.GetMyProfileResponseData;
+import com.softgroup.profile.api.router.ProfileRequestHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProfCmdGetMyProfileHandler
+        extends AbstractRequestHandler<GetMyProfileRequestData, GetMyProfileResponseData>
+        implements ProfileRequestHandler
+{
+    private static final String PROF_CMD_GET_MY_PROFILE_NAME = "get_my_profile";
+
+    @Override
+    public String getName() {
+        return PROF_CMD_GET_MY_PROFILE_NAME;
+    }
+
+    @Override
+    public Response<GetMyProfileResponseData> commandHandle(Request<GetMyProfileRequestData> request) {
+        return null;
+    }
+}
