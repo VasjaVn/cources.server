951c9ca2034030cb32e2951d69d20c280e852247
VasjaVn <euro2012tovinnitsa@gmail.com>
2017-03-09 16:36:10 +0200

    fix name class

--- softgroup/services/profile/profile-api/src/main/java/com/softgroup/profile/api/message/GetMyProfileRequestData.java
+++ softgroup/services/profile/profile-api/src/main/java/com/softgroup/profile/api/message/GetMyProfileRequestData.java
@@ -2,6 +2,6 @@ package com.softgroup.profile.api.message;
 
 import com.softgroup.common.protocol.RequestData;
 
-public class SetMyProfileRequestData implements RequestData {
+public class GetMyProfileRequestData implements RequestData {
     private static final long serialVersionUID = -6954242298890914144L;
 }
