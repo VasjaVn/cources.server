f41e766620f91d71066284257080cf04c0f4b9f7
VasjaVn <euro2012tovinnitsa@gmail.com>
2017-03-13 11:22:57 +0200

    remove command handler factory from class ProfileTypeRouterHandler

--- softgroup/services/profile/profile-impl/src/main/java/com/softgroup/profile/impl/router/ProfileTypeRouterHandler.java
+++ softgroup/services/profile/profile-impl/src/main/java/com/softgroup/profile/impl/router/ProfileTypeRouterHandler.java
@@ -1,32 +1,19 @@
 package com.softgroup.profile.impl.router;
 
-import com.softgroup.common.protocol.Request;
-import com.softgroup.common.protocol.Response;
+import com.softgroup.common.router.api.AbstractRouterHandler;
+import com.softgroup.profile.api.router.ProfileRequestHandler;
 import com.softgroup.profile.api.router.ProfileRouterHandler;
-import com.softgroup.profile.impl.factory.ProfileCommandHandlerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class ProfileTypeRouterHandler implements ProfileRouterHandler
+public class ProfileTypeRouterHandler
+        extends AbstractRouterHandler<ProfileRequestHandler>
+        implements ProfileRouterHandler
 {
     private static final String PROFILE_TYPE_ROUTER_NAME = "profile";
-
-    @Autowired
-    private ProfileCommandHandlerFactory commandHandlerFactory;
 
     @Override
     public String getName() {
         return PROFILE_TYPE_ROUTER_NAME;
-    }
-
-    @Override
-    public Response<?> handle(Request<?> request) {
-        return commandHandlerFactory.getHandler( request ).handle( request );
-    }
-
-    @Override
-    public String getRouteKey(Request<?> msg) {
-        return null;
     }
 }
