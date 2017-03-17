43e5d77f43f5fa740543da3510d6beb8c71b7693
VasjaVn <euro2012tovinnitsa@gmail.com>
2017-03-17 14:31:37 +0200

    add router type handler factory

---
+++ softgroup/basis/common-router/common-router-impl/src/main/java/com/softgroup/common/router/impl/factory/RouterTypeHandlerFactory.java
@@ -0,0 +1,9 @@
+package com.softgroup.common.router.impl.factory;
+
+import com.softgroup.common.factory.TypeHandlerFactory;
+import com.softgroup.common.router.api.CommonRouterHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RouterTypeHandlerFactory extends TypeHandlerFactory<CommonRouterHandler> {
+}
