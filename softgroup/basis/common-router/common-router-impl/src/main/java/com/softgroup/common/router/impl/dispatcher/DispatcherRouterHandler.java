0c7bd29b092122d88a273a971e982fa42a886662
VasjaVn <euro2012tovinnitsa@gmail.com>
2017-03-17 15:05:33 +0200

    add return bad request in method handle()

--- softgroup/basis/common-router/common-router-impl/src/main/java/com/softgroup/common/router/impl/dispatcher/DispatcherRouterHandler.java
+++ softgroup/basis/common-router/common-router-impl/src/main/java/com/softgroup/common/router/impl/dispatcher/DispatcherRouterHandler.java
@@ -3,6 +3,7 @@ package com.softgroup.common.router.impl.dispatcher;
 import com.softgroup.common.factory.HandlerFactory;
 import com.softgroup.common.protocol.Request;
 import com.softgroup.common.protocol.Response;
+import com.softgroup.common.protocol.ResponseBuilder;
 import com.softgroup.common.router.api.CommonRouterHandler;
 import com.softgroup.common.router.api.RouterHandler;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,7 +23,12 @@ public class DispatcherRouterHandler implements RouterHandler {
 
     @Override
     public Response<?> handle(Request<?> msg) {
-        return routerHandlerFactory.getHandler(msg).handle(msg);
+        RouterHandler routerHandler = routerHandlerFactory.getHandler(msg);
+        if ( routerHandler == null ) {
+            /* Rewrite this after create utility (factory) for create response*/
+            return new ResponseBuilder(msg).withCode(400).withMessage("Bad request").build();
+        }
+        return routerHandler.handle(msg);
     }
 
     @Override
